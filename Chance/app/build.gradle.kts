e0e2bc352dbfba4e66045107f94e59a79f4ec5a4
lamersc <me@lamersc.com>
2025-11-04 17:28:39 -0700

    UI work

--- Chance/app/build.gradle.kts
+++ Chance/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation("com.google.android.flexbox:flexbox:3.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
