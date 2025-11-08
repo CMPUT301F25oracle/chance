364b566d8c5fd9124ca5bf21e12db6b22b47b41f
qzimeng-art <qzimeng@ualberta.ca>
2025-11-07 23:38:52 -0700

    More Test cases added
    Co-authored-by: lamersc <me@lamersc.com>
    Co-authored-by: Chaidst <cthakur@ualberta.ca>

---
+++ Chance/app/src/test/java/com/example/chance/NotificationTest.java
@@ -0,0 +1,28 @@
+package com.example.chance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.app.Notification;
+
+
+// Unit tests for the Notification class
+// For the four notification tests, we will implement them
+public class NotificationTest {
+
+    private Notification notification = new Notification();
+
+    @Test
+    public void opt_out_of_notifications() {}
+
+    @Test
+    public void receive_loss_notifications() {}
+
+    @Test
+    public void receive_win_notifications() {}
+
+    @Test
+    public void send_notifications_to_chosen_entrants() {}
+
+}
