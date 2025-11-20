5970596df5c20ac201632356bc2a6e5ed0d858b7
lamersc <me@lamersc.com>
2025-11-19 19:57:35 -0700

    Set up created events filter

--- Chance/app/src/main/java/com/example/chance/controller/DataStoreManager.java
+++ Chance/app/src/main/java/com/example/chance/controller/DataStoreManager.java
@@ -343,7 +343,7 @@ public class DataStoreManager {
      * @return
      */
     public Event createEvent(String name, String location, int capacity, double price, String description, Date startDate, Date endDate, String organizerUserName) {
-        Event new_event = new Event(name, location, capacity, price, description, startDate, endDate, organizerUserName);
+        Event new_event = new Event(name, location, capacity, price, description, startDate, endDate, organizerUserName, 0);
         db.setDocument("events", new_event.getID(), new_event, (s)->{}, (s)->{});
         return new_event;
     }
--- Chance/app/src/main/java/com/example/chance/views/CreatedEvents.java
+++ Chance/app/src/main/java/com/example/chance/views/CreatedEvents.java
@@ -6,12 +6,24 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.chance.model.Event;
+
+import java.util.List;
+import java.util.Objects;
+
 public class CreatedEvents extends MultiPurposeEventSearchScreen {
 
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        cvm.getEvents().observe(getViewLifecycleOwner(), this::submitList);
+        cvm.getCurrentUser().observe(getViewLifecycleOwner(), user -> {
+            cvm.getEvents().observe(getViewLifecycleOwner(), events -> {
+                List<Event> userEvents = events
+                    .stream()
+                    .filter(event -> Objects.equals(user.getID(), event.getOrganizerUID())).toList();
+                submitList(userEvents);
+            });
+        });
     }
 }
