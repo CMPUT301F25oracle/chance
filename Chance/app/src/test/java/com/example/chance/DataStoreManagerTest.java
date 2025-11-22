1746277128329fcebf469f57b842011437d99fca
Rishit Singh Grover <rishitgrover4@gmail.com>
2025-11-21 22:39:21 -0700

    Add EventTest skeleton with 12 test methods

---
+++ Chance/app/src/test/java/com/example/chance/DataStoreManagerTest.java
@@ -0,0 +1,84 @@
+package com.example.chance;
+
+import com.example.chance.controller.DataStoreManager;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for DataStoreManager
+ */
+public class DataStoreManagerTest {
+
+    private DataStoreManager dataStoreManager;
+
+    @Before
+    public void setUp() {
+        dataStoreManager = DataStoreManager.getInstance();
+    }
+
+    @Test
+    public void testGetInstance() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testCreateNewUser() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testAuthenticateUser() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testGetAuthenticatedUser() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testLogoutUser() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testCreateNewEvent() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testGetEvent() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testGetAllEvents() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testUpdateEvent() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testJoinWaitingList() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testLeaveWaitingList() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testAcceptInvitation() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testDeleteEventBanner() {
+        // TODO: Implement test
+    }
+}
