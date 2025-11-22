1171a1df27011ccc4fb8f7900d9ffb0f6191a514
Rishit Singh Grover <rishitgrover4@gmail.com>
2025-11-21 22:42:16 -0700

    Add ProfileControllerTest skeleton with 9 test methods

---
+++ Chance/app/src/test/java/com/example/chance/ProfileControllerTest.java
@@ -0,0 +1,64 @@
+package com.example.chance;
+
+import com.example.chance.controller.ProfileController;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for ProfileController
+ */
+public class ProfileControllerTest {
+
+    private ProfileController profileController;
+
+    @Before
+    public void setUp() {
+        profileController = new ProfileController();
+    }
+
+    @Test
+    public void testGetEntrant() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testUpdateEntrant() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testDeleteEntrant() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testGetOrganizer() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testUpdateOrganizer() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testDeleteOrganizer() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testGetAdministrator() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testUpdateAdministrator() {
+        // TODO: Implement test
+    }
+
+    @Test
+    public void testDeleteAdministrator() {
+        // TODO: Implement test
+    }
+}
